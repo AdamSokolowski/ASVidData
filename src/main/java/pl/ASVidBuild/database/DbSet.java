a82f83587e0a639baf7f09d45368301cdcd58bb2
AdamSokolowski <a.sokolowski84@gmail.com>
2019-01-15 12:01:17 +0100

    Readme and MySQL credentials update

--- src/main/java/pl/ASVidBuild/database/DbSet.java
+++ src/main/java/pl/ASVidBuild/database/DbSet.java
@@ -4,5 +4,5 @@ public interface DbSet {
 	public static final String DB_NAME = "asviddata";
 	public static final String DB_URL = "jdbc:mysql://localhost:3306/"+DB_NAME+"?useSSL=false&useUnicode=true&characterEncoding=utf8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT";
 	public static final String DB_USER = "root";
-	public static final String DB_PASS = "coderslab";
+	public static final String DB_PASS = "pass";
 }
